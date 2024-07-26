a03c0a4c97ba920f501ef7a899c07d6fb1a6a84f
bs1516h <jianhan_liu123@163.com>
2024-07-26 22:40:35 +0800

    step02

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.22.5
+
+require github.com/danicat/simpleansi v0.0.0-20200320095209-8cd0472eec8b // indirect
--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+
+	"github.com/danicat/simpleansi"
 )
 
 var maze []string
 
-func loadMase(filePath string) error {
+func loadMaze(filePath string) error {
 	f, err := os.Open(filePath)
 	if err != nil {
 		return err
@@ -26,16 +29,54 @@ func loadMase(filePath string) error {
 }
 
 func printScreen() {
+	simpleansi.ClearScreen()
 	for _, line := range maze {
 		fmt.Println(line)
 	}
 }
 
+func initialize() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatal("unable to activate cbreak mode:", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatal("unable to restore cooked mode:", err)
+	}
+}
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+
+	return "", nil
+}
+
 func main() {
 	// initialize game
+	initialize()
+	defer cleanup()
 
 	// load resources
-	err := loadMase("maze01.txt")
+	err := loadMaze("maze01.txt")
 	if err != nil {
 		log.Println("failed to load maze", err)
 		return
@@ -46,14 +87,19 @@ func main() {
 		// update screen
 		printScreen()
 		// process input
-
+		input, err := readInput()
+		if err != nil {
+			log.Print("error reading input:", err)
+			break
+		}
 		// process movement
 
 		// process collisions
 
 		// Temp: break infinite loop
-		break
-
+		if input == "ESC" {
+			break
+		}
 		// repeat
 	}
 }
