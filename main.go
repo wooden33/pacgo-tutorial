62b9d69ad677d6ce93ee04e22ee250d88443752a
bs1516h <jianhan_liu123@163.com>
2024-07-26 21:18:43 +0800

    step01

--- main.go
+++ main.go
@@ -1,16 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var maze []string
+
+func loadMase(filePath string) error {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+
+	return nil
+}
+
+func printScreen() {
+	for _, line := range maze {
+		fmt.Println(line)
+	}
+}
 
 func main() {
 	// initialize game
 
 	// load resources
+	err := loadMase("maze01.txt")
+	if err != nil {
+		log.Println("failed to load maze", err)
+		return
+	}
 
 	// game loop
 	for {
 		// update screen
-
+		printScreen()
 		// process input
 
 		// process movement
@@ -18,7 +52,6 @@ func main() {
 		// process collisions
 
 		// Temp: break infinite loop
-		fmt.Println("Hello, Pac Go!")
 		break
 
 		// repeat
