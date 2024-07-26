f9b447045f8434ff5173d88639906b65ab40f348
bs1516h <jianhan_liu123@163.com>
2024-07-26 20:52:27 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// initialize game
+
+	// load resources
+
+	// game loop
+	for {
+		// update screen
+
+		// process input
+
+		// process movement
+
+		// process collisions
+
+		// Temp: break infinite loop
+		fmt.Println("Hello, Pac Go!")
+		break
+
+		// repeat
+	}
+}
